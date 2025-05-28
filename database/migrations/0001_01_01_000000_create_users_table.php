f090201b42d6f5c946a30b84e2e350198a4f5976
Ghanem Abdelaziz <ghanemabdelaziz.business@gmail.com>
2025-05-28 14:48:20 +0300

    feat: add booking_link and timezone to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('booking_link')->unique()->nullable();
+            $table->string('time_zone')->default('UTC');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
